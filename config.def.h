070391f87dcc1edcd765ce04ad938a1d37e89898
Yuxuan Yan <yanyuxuan@cau.edu.cn>
2023-01-10 12:39:41 +0800

    add goldendict wine-wechat rules

--- config.def.h
+++ config.def.h
@@ -31,13 +31,14 @@ static const char *colors[][3]      = {
 static const char *tags[] = { "", "", "", "", "5", "6", "7", "﬜", "" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	/* { "Gimp",     NULL,       NULL,       0,            1,           -1 }, */
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+        /* xprop(1):
+	 *          *      WM_CLASS(STRING) = instance, class
+	 *                   *      WM_NAME(STRING) = title
+	 *                            */
+        /* class              instance        title       tags mask     isfloating   monitor */
+        { "GoldenDict",       NULL,           NULL,       1 << 7,       0,           -1 },
+        { "Wine",             "wechat.exe",   NULL,       1 << 8,       0,           -1 },
+        { "Wine",             "explorer.exe", NULL,       1 << 8,       1,           -1 }
 };
 
 /* layout(s) */
