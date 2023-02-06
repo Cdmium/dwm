04dbbcd15e15bf1a9c299b5ae786ea129e68a620
Yuxuan Yan <yanyuxuan@cau.edu.cn>
2023-02-06 15:47:37 +0800

    set speedcrunch scratchpad

--- config.def.h
+++ config.def.h
@@ -24,12 +24,12 @@ typedef struct {
 } Sp;
 const char *spcmd1[] = {"st", "-n", "spterm", "-g", "256x60", NULL };
 const char *spcmd2[] = {"st", "-n", "spfm", "-g", "256x60", "-e", "ranger", NULL };
-const char *spcmd3[] = {"keepassxc", NULL };
+const char *spcmd3[] = {"speedcrunch", NULL };
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"spterm",      spcmd1},
 	{"spranger",    spcmd2},
-	{"keepassxc",   spcmd3},
+	{"speedcrunch",   spcmd3},
 };
 
 /* tagging */
@@ -40,11 +40,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",	  NULL,			NULL,		0,				1,			 -1 },
-	{ "Firefox",  NULL,			NULL,		1 << 8,			0,			 -1 },
+	{ "Gimp",	  NULL,			NULL,		0,			1,			 -1 },
+	{ "Firefox",      NULL,			NULL,		1 << 8,			0,			 -1 },
 	{ NULL,		  "spterm",		NULL,		SPTAG(0),		1,			 -1 },
 	{ NULL,		  "spfm",		NULL,		SPTAG(1),		1,			 -1 },
-	{ NULL,		  "keepassxc",	NULL,		SPTAG(2),		0,			 -1 },
+	{ NULL,		  "speedcrunch",	NULL,		SPTAG(2),		1,			 -1 },
 };
 
 /* layout(s) */
