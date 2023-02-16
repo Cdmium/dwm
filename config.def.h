6b0b4fe31605cf1d04f7f1592d65f173e276b7c2
Yuxuan Yan <yanyuxuan@cau.edu.cn>
2023-02-17 00:40:48 +0800

    make font bigger

--- config.def.h
+++ config.def.h
@@ -1,20 +1,20 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 3;        /* gaps between windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx     = 4;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
 
 static const char *fonts[]          = {
-	"Symbols Nerd Font:pixelsize=12:type=2048-em:antialias=true:autohint=true",
-	"Noto Sans Mono CJK SC:size=9:type=Regular:antialias=true:autohint=true",
-	"JoyPixels:pixelsize=10:type=Regular:antialias=true:autohint=true"
+	"Symbols Nerd Font:pixelsize=15:type=2048-em:antialias=true:autohint=true",
+	"Noto Sans Mono CJK SC:size=11:type=Regular:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=13:type=Regular:antialias=true:autohint=true"
 };
 
-static const char dmenufont[]       = "Noto Sans Mono CJK SC:size=9:type=Regular:antialias=true:autohint=true";
+static const char dmenufont[]       = "Noto Sans Mono CJK SC:size=11:type=Regular:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
